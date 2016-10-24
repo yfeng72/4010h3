997a2ca2a17fd9ac130542996b0ab665aaf5fce7
dstewart1023 <dstewart1023@gmail.com>
2016-10-24 01:50:12 -0400

    Finished createNetwork
    Need to optimize it so that large values for numNodes can run relatively
    quickly.

--- graphgen.c
+++ graphgen.c
@@ -5,7 +5,7 @@
 /**
  * Instantiates and builds the network
  */
-//LinkedList **createNetwork( int numNodes );
+LinkedList *createNetwork( int numNodes );
 
 /**
  * Returns the number of connections a node has.
@@ -16,6 +16,11 @@ int getDegree( LinkedList *connections );
  * Return the sum of all degree nodes.
  */
 int degreeSum( LinkedList* network, int numNodes);
+
+/**
+ * Updates the probability of a node connection as nodes are added
+ */
+double updateProbabilities(LinkedList* network, int numNodes, int node);
 
 /**
  * Save the network to a .csv file
@@ -48,9 +53,21 @@ int degreeSum( LinkedList* network, int numNodes) {
 	return total;
 }
 
+double updateProbabilities(LinkedList* network, int numNodes, int node) {
+	double degree = getDegree(find(network, node)->neighbors);
+	double allDegrees = degreeSum(network, numNodes);
+	return degree / allDegrees;
+}
+
 
 LinkedList* createNetwork( int numNodes ) {
 	int i;
+	int connect = 0;
+	int next = 3;
+	srand(time(NULL));
+	double chance;
+	double probability[numNodes];
+	double percent[numNodes];
 	LinkedList* network = newList();
 
 	//Create the LinkedList describing the network; fill with nodes
@@ -64,6 +81,38 @@ LinkedList* createNetwork( int numNodes ) {
 	link(find(network, 0), find(network, 2));
 	link(find(network, 1), find(network, 2));
 
+	//Initialize arrays
+	for(i = 0; i < numNodes; i++) {
+		probability[i] = 0.0;
+		percent[i] = 0.0;
+	}
+
+
+	//Algorithmically connect new nodes until network is complete
+	while(next < numNodes) {
+		connect = 0;
+		chance = rand() / (RAND_MAX + 1.0);
+		for(i = 0; i < numNodes; i++) {
+			probability[i] = updateProbabilities(network, numNodes, i);
+			if(i == 0) {
+				percent[i] = 0.0;
+			} else if(probability[i] == 0){
+				percent[i] = 1.0;
+			} else {
+				percent[i] = percent[i - 1] + probability[i];
+			}
+		}
+
+		for(i = 0; i < numNodes; i++) {
+			if(percent[i] == 1.0){
+				break;
+			} else if(chance > percent[i + 1]) {
+				connect = i;
+			}
+		}
+		link(find(network, next), find(network, connect));
+		next++;
+	}
 
 
 	return network;
@@ -73,9 +122,11 @@ LinkedList* createNetwork( int numNodes ) {
  * Checked that degreeSum works properly; should output 6
  */
 int main( int argc, char **argv ) {
+	int i;
     printf( "testing\n" );
-    LinkedList* network = createNetwork(5);
-    printf("%d\n", degreeSum(network, 5));
+    LinkedList* network = createNetwork(100);
+    for(i = 0; i < 100; i++){
+    	printf("%d, %d\n", i, getDegree(find(network, i)->neighbors));
+    }
     deleteList(network);
-
 }
