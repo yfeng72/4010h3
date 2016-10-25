db8272e8867aa1358006c79a107d276789e681f6
yfeng72 <fengy@gatech.edu>
2016-10-25 12:51:16 -0400

    Added comments and updated readme file

--- README.md
+++ README.md
@@ -1,4 +1,21 @@
-##
-
 CX 4010 Project 3
+10/28/16
+
+# Authors
+
 Yu Feng, Douglas Stewart
+
+# Compilation and Run Instructions
+
+To compile the GraphGen application:
+gcc -o graphgen linkedList.c linkedList.h graphgen.c -std=c99 -lm
+
+To compile the Analysis application:
+gcc -o analysis linkedList.c linkedList.h analysis.c -std=c99 -lm
+
+To run the graphgen application:
+./graphgen <# of nodes> [-h <filename>]
+
+To run the analysis application:
+./analysis <CSV filename> [-o <output filename>]
+
--- analysis.c
+++ analysis.c
@@ -3,6 +3,12 @@
 #include <stdlib.h>
 #include <string.h>
 
+/**
+ * Parses the CSV file to a graph
+ * @param filename : the name of the CSV file to be parsed, in the format of each line representing a node and its neighbors
+ * the first cell being the node number of the node, and the remaining cells its neighbors
+ * @return : Array of GraphNodes that are connected, the first node holds information on the number of nodes
+ */
 GraphNode **parseCsv( char *filename ) {
     int nodeCount = 0;
     //Get number of nodes
@@ -22,7 +28,7 @@ GraphNode **parseCsv( char *filename ) {
     char *csvContents = calloc( sizeof( char ), (size_t) fileLength );
     fread( csvContents, 1, (size_t) fileLength, csvFile );
     fclose( csvFile );
-
+    //Allocates space
     char **csvLines = (char **) calloc( sizeof( char * ), (size_t) nodeCount );
     int count = 0;
     for ( char *line = strtok( csvContents, "\n" ); line != NULL; line = strtok(NULL, "\n" )) {
@@ -31,6 +37,7 @@ GraphNode **parseCsv( char *filename ) {
         count++;
     }
     int nodeIndex = 1;
+    //Parses each line of csv
     for ( count = 0; count < nodeCount; count++ ) {
         int neighborIndex = 0;
         int curNode = -1;
@@ -40,8 +47,7 @@ GraphNode **parseCsv( char *filename ) {
                     nodes[nodeIndex] = newGraphNode( atoi( num ) + 1 );
                 }
                 curNode = atoi( num ) + 1;
-            }
-            else {
+            } else {
                 int targetNode = atoi( num ) + 1;
                 if ( !nodes[targetNode] ) {
                     nodes[targetNode] = newGraphNode( targetNode );
@@ -51,27 +57,40 @@ GraphNode **parseCsv( char *filename ) {
             neighborIndex++;
         }
     }
+    //Free memory used for parsing CSV file
     for ( int i = 0; i < nodeCount; i++ ) free( csvLines[i] );
     free( csvLines );
     free( csvContents );
     return nodes;
 }
 
+/**
+ * Parses a node of the existing graph, returns a linked list of the nodes farthest away from
+ * the current node
+ * @param nodes : the graph
+ * @param index : index of the current node on the graph
+ * @return : the linked list described above, the head of the linkedlist holds the distance value,
+ * the tail of the linkedlist holds the node number of the current node
+ */
 LinkedList *parseDistance( GraphNode **nodes, int index ) {
+    //Extract information of the graph
     int nodeCount = nodes[0]->value;
+    //distances array holds array of nodes with the index as the distance to the origin node
     LinkedList **distances = (LinkedList **) calloc( sizeof( LinkedList * ), (size_t) nodeCount );
     for ( int i = 0; i < nodeCount; i++ ) {
         distances[i] = NULL;
     }
+    //The node itself is appended to distances[0]
     distances[0] = newList();
     append( distances[0], newGraphNode( nodes[index]->value ));
+    //If a node is marked, it has been visited
     nodes[index]->marked = 1;
     int i = 1;
+    //Expand the distance by 1 for each iteration, until there are no more nodes
     for ( ; i < nodeCount; i++ ) {
         if ( !distances[i - 1] || isEmpty( distances[i - 1] )) {
             break;
-        }
-        else {
+        } else {
             distances[i] = newList();
         }
         Node *curNode = distances[i - 1]->head->next;
@@ -80,6 +99,7 @@ LinkedList *parseDistance( GraphNode **nodes, int index ) {
             Node *neighbor = neighbors->head->next;
             while ( neighbor != neighbors->tail ) {
                 GraphNode *neighborGraphNode = neighbor->node;
+                //Add unvisited neighbors to the distance list
                 if ( !neighborGraphNode->marked ) {
                     append( distances[i], newGraphNode( neighborGraphNode->value ));
                     neighborGraphNode->marked = 1;
@@ -89,37 +109,44 @@ LinkedList *parseDistance( GraphNode **nodes, int index ) {
             curNode = curNode->next;
         }
     }
+    //Copy the item (LinkedList) at the maximum valid index of the distances array, because distances
+    //array is going to be freed
     LinkedList *ret = newList();
     ret->head->node = newGraphNode( i - 2 );
     ret->tail->node = newGraphNode( nodes[index]->value );
     for ( Node *curNode = distances[i - 2]->head->next; curNode != distances[i - 2]->tail; curNode = curNode->next ) {
         append( ret, curNode->node );
     }
+    //Mark all nodes as unvisited for future calls
     for ( int j = 1; j <= nodeCount; j++ ) {
         nodes[j]->marked = 0;
     }
+    //Free all allocated space, except for the return value
     for ( int j = 0; j < nodeCount; j++ ) {
         if ( distances[j] ) {
             deleteList( distances[j] );
         }
     }
     free( distances );
+
     return ret;
 }
 
 int main( int argc, char **argv ) {
+    //Check if argument count is valid
     if ( argc != 2 && argc != 4 ) {
         printf( "Illegal Arguments, please check input\n" );
         return 1;
     }
     int outputToFile = argc == 4 ? 1 : 0;
-
+    //Parse CSV file specified in the arguments
     GraphNode **nodes = parseCsv( argv[1] );
     int nodeCount = nodes[0]->value;
     int diameter = 0;
     char output[262144] = "";
     for ( int i = 1; i <= nodeCount; i++ ) {
         char line[65536] = "";
+        //Build the output string line by line
         LinkedList *curResult = parseDistance( nodes, i );
         sprintf( line, "Node %d: Maximum Distance - %d, Nodes at Max Distance:", curResult->tail->node->value - 1,
                  curResult->head->node->value );
@@ -132,25 +159,28 @@ int main( int argc, char **argv ) {
         strcat( line, "\n" );
         strcat( output, line );
         int curMax = curResult->head->node->value;
+        //Diameter is the maximum of all max distances on each graph node
         diameter = curMax > diameter ? curMax : diameter;
-
+        //Free allocated space
         for ( Node *curNode = curResult->head;
               curNode; curNode = curNode->next ) {
             delete( curNode->node );
         }
         deleteList( curResult );
     }
+    //Free allocated space
     for ( int i = 0; i <= nodeCount; i++ ) {
         delete( nodes[i] );
     }
     free( nodes );
     char finalOutput[262144] = "";
+    //Append diameter to the output string
     sprintf( finalOutput, "Diameter: %d\n", diameter );
     strcat( finalOutput, output );
+    //Print to console or output to file depending on the arguments passed in
     if ( !outputToFile ) {
         printf( finalOutput );
-    }
-    else {
+    } else {
         FILE *outputFile = fopen( argv[3], "w" );
         fputs( finalOutput, outputFile );
         fclose( outputFile );
@@ -160,4 +190,5 @@ int main( int argc, char **argv ) {
         strcat( outputLine, " generated\n" );
         printf( outputLine );
     }
+    return 0;
 }
